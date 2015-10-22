fb42a5f7ea495b5521ea47eeaee39e0ec9bf0a64
tps-80 <thomaspsorensen@gmail.com>
2015-10-22 16:04:42 -0700

    Implemented NowPlaying view

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -5,16 +5,19 @@ var AppView = Backbone.View.extend({
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
     this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
+    this.nowPlayingView = new NowPlayingView({model: this.model.get('currentSong')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model) {
       this.playerView.setSong(model.get('currentSong'));
+      this.nowPlayingView.setSong(model.get('currentSong'));
     }, this);
   },
 
   render: function() {
     return this.$el.html([
+      this.nowPlayingView.$el,
       this.playerView.$el,
       this.libraryView.$el,
       this.songQueueView.$el
---
+++ client/views/NowPlayingView.js
@@ -0,0 +1,26 @@
+var NowPlayingView = Backbone.View.extend({
+
+  // HTML5 (native) audio tag is being used
+  // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
+  
+  
+  initialize: function() {
+  
+    var context = this;
+    this.$el.on('ended', function(){
+      this.$el.empty();
+
+    });
+  },
+
+  setSong: function(song) {
+    this.model = song;
+    this.render();
+  },
+
+  render: function() {
+    this.$el.empty();
+    return this.$el.append('<span> Now Playing: ' + this.model.get('artist') + ' ' + this.model.get('title') + '</span>');
+  }
+
+});
